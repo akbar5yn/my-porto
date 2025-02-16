a186995ae443d14565b8f6d068a2dfdc605986c6
Akbar <akbar5yn.developer@gmail.com>
2025-02-16 19:04:47 +0700

    Update Profile Header to change name and description

--- src/components/molecules/ProfileHeader.tsx
+++ src/components/molecules/ProfileHeader.tsx
@@ -3,10 +3,10 @@ import Typography from "../atoms/Typography";
 const ProfileHeader = () => {
     return (
         <div className="space-y-2">
-            <Typography tag="h1" variantClass="title" className="text-red-400">Brittany Chiang</Typography>
-            <Typography tag="h2" variantClass="subtitle">Front End Engineer</Typography>
+            <Typography tag="h1" variantClass="title">Akbar Pratama Suryamin</Typography>
+            <Typography tag="h2" variantClass="subtitle">Web Developer</Typography>
             <Typography tag="p" variantClass="body">
-                I build accessible, pixel-perfect digital experiences for the web.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt neque, accusamus similique quidem at consequatur suscipit assumenda quas voluptas tempora?
             </Typography>
         </div>
     );

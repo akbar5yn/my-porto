39c31f5f4abc67ff38503ff49067f767c02c4afb
Akbar <akbar5yn.developer@gmail.com>
2025-02-16 18:53:56 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ProfileHeader from './components/molecules/ProfileHeader'
+import './assets/css/main.css'
+
+
+function App() {
+
+  return (
+    <main>
+      <ProfileHeader />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/atoms/Typography.tsx
@@ -0,0 +1,23 @@
+import { JSX, ReactNode } from "react";
+
+type TypographyProps = {
+    tag?: keyof JSX.IntrinsicElements;
+    variantClass?: "title" | "subtitle" | "body";
+    className?: string;
+    children: ReactNode;
+}
+
+const Typography = (props: TypographyProps) => {
+    const { tag: Tag = "p", variantClass = "body", className = "", children } = props
+    const typographyClass = {
+        title: "text-4xl font-bold",
+        subtitle: "text-xl font-semibold",
+        body: "text-base text-gray-400",
+    };
+    return (
+        <Tag className={`${typographyClass[variantClass]} ${className}`}>{children}</Tag>
+    );
+
+}
+
+export default Typography;

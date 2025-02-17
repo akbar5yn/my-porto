25b10ae4816857f00f755e0d6cd09ede42bf9b91
Akbar <akbar5yn.developer@gmail.com>
2025-02-17 18:42:43 +0700

    mengubah pemangginal component dan menambahkan beberapa styleling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import ProfileHeader from './components/molecules/ProfileHeader'
 import './assets/css/main.css'
+import Layout from './components/templates/Layout'
 
 
 function App() {
 
   return (
-    <main>
-      <ProfileHeader />
+    <main className='h-screen flex items-center justify-center py-32'>
+      <Layout />
     </main>
   )
 }

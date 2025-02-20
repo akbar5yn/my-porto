30d406a15aa06f23767a31d9d4cf6614c2895174
Akbar <akbar5yn.developer@gmail.com>
2025-02-20 15:49:50 +0700

    membuat molecules SocialIcons

---
+++ src/components/molecules/SocialIcons.tsx
@@ -0,0 +1,16 @@
+import Icon from "../atoms/Icon"
+import { SiRefinedgithub } from "react-icons/si";
+import { CiLinkedin } from "react-icons/ci";
+import { TiSocialInstagram } from "react-icons/ti";
+
+const SocialIcons = () => {
+    return (
+        <div className="flex flex-col bg-white rounded-br-lg gap-2 p-2 relative container-image">
+            <Icon><SiRefinedgithub className="w-7 h-7" /></Icon>
+            <Icon><CiLinkedin className="w-7 h-7" /></Icon>
+            <Icon><TiSocialInstagram className="w-7 h-7" /> </Icon>
+        </div>
+    )
+}
+
+export default SocialIcons

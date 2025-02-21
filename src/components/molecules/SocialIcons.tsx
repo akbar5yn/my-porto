26f3777fa793253902c8bb5a8f493c2ae33531e0
Akbar <akbar5yn.developer@gmail.com>
2025-02-21 20:00:33 +0700

    menambahkan styleling border image

--- src/components/molecules/SocialIcons.tsx
+++ src/components/molecules/SocialIcons.tsx
@@ -5,7 +5,7 @@ import { TiSocialInstagram } from "react-icons/ti";
 
 const SocialIcons = () => {
     return (
-        <div className="flex flex-col bg-white rounded-br-lg gap-2 p-2 relative container-image">
+        <div className="flex flex-col bg-white rounded-br-lg gap-2 p-2 relative container-image border-r border-b">
             <Icon><SiRefinedgithub className="w-7 h-7" /></Icon>
             <Icon><CiLinkedin className="w-7 h-7" /></Icon>
             <Icon><TiSocialInstagram className="w-7 h-7" /> </Icon>

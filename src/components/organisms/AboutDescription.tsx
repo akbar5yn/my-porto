c8ce9c6ae7573b230b67f1982e1d10310d37b2f1
Akbar <akbar5yn.developer@gmail.com>
2025-02-21 20:00:50 +0700

    melakukan prafrase AboutDescription

--- src/components/organisms/AboutDescription.tsx
+++ src/components/organisms/AboutDescription.tsx
@@ -1,15 +1,15 @@
 import Typography from "../atoms/Typography";
-import Card from "../molecules/Card";
 
 const AboutDescription = () => {
     return (
         <section className="flex flex-col gap-2 text-justify">
             <Typography tag="h1" variantClass="title">About Me</Typography>
             <Typography tag="p" variantClass="body">
-                Saya adalah seroang <span className="font-bold">web developer</span>, yang dimana saya mengembangkan antarmuka web yang modern dan responsif yang memadukan dengan pengalaman pengguna sehingga
-                menghasilkan tampilan yang intuitif, menarik, dan fungsional. Saya berfokus pada detail, performa, dan kemudahan aksesibilitas, sehingga setiap pengguna dapat
-                merasakan pengalaman yang optimal dalam menggunakan aplikasi atau website yang saya buat.
+                Saya adalah seorang <span className="font-bold">Web Developer</span> yang mengembangkan antarmuka web modern dan responsif.
+                Dengan perpaduan desain intuitif dan pengalaman pengguna yang optimal, saya menciptakan tampilan yang menarik serta fungsional.
+                Fokus utama saya adalah detail, performa, dan aksesibilitas, sehingga setiap pengguna dapat menikmati pengalaman terbaik saat menggunakan aplikasi atau website yang    saya buat.
             </Typography>
+
         </section>
     );
 };

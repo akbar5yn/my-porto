9aa0ea35ff835b771c9c44d96e4c451e3f641a01
Akbar <akbar5yn.developer@gmail.com>
2025-03-14 01:18:26 +0700

    membuat interface data

--- src/components/organisms/ProfileSection.tsx
+++ src/components/organisms/ProfileSection.tsx
@@ -1,6 +1,25 @@
 import Typography from "../atoms/Typography";
 import profile from "../../assets/img/profile_pic.jpg"
 import SocialIcons from "../molecules/SocialIcons";
+import Nav from "../molecules/Nav";
+
+const navItem = [
+    { label: "ABOUT", to: "/about" },
+    { label: "EXPERIENCE", to: "/experience" },
+    { label: "PROJECTS", to: "/projects" }
+]
+
+interface interfaceProfileData {
+    name: string
+    job: string
+    description: string
+}
+
+const profileData: interfaceProfileData = {
+    name: "Akbar Pratama Suryamin",
+    job: "Web Developer",
+    description: "Mengembangkan aplikasi web yang responsif dan intuitif dengan teknologi modern untuk pengalaman pengguna yang optimal."
+}
 
 
 const ProfileSection = () => {
@@ -12,12 +31,13 @@ const ProfileSection = () => {
                     <SocialIcons />
                 </div>
             </section>
-            <section className="absolute bottom-0 bg-white space-y-2 z-50 bayangan p-5 border border-gray-200 rounded-2xl">
-                <Typography tag="h1" variantClass="title">Akbar Pratama Suryamin</Typography>
-                <Typography tag="h2" variantClass="subtitle">Web Developer</Typography>
+            <section className="absolute w-full bottom-0 bg-white space-y-2 z-50 bayangan p-5 border border-gray-200 rounded-2xl">
+                <Typography tag="h1" variantClass="title">{profileData.name}</Typography>
+                <Typography tag="h2" variantClass="subtitle">{profileData.job}</Typography>
                 <Typography tag="p" variantClass="body">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt neque, accusamus similique quidem at consequatur suscipit assumenda quas voluptas tempora?
+                    {profileData.description}
                 </Typography>
+                <Nav links={navItem} className="mt-4"></Nav>
             </section>
         </div>
     );

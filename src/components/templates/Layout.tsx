fb0568fe389f79df7299fadc5a9208bbb63ccefa
Akbar <akbar5yn.developer@gmail.com>
2025-03-14 01:18:46 +0700

    menambahkan motion

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -1,19 +1,29 @@
 import { ReactNode } from "react"
 import ProfileSection from "../organisms/ProfileSection"
-
+import { motion } from "motion/react"
 type LayoutProps = {
     children: ReactNode
 }
 
 const Layout = (props: LayoutProps) => {
+
     const { children } = props
     return (
         <div className="layout h-full grid max-w-screen-2xl grid-cols-[40%_auto] gap-10 px-10">
             <ProfileSection></ProfileSection>
             <div className="h-full main-content w-full overflow-hidden">
-                <main className="h-full">{children}</main>
+                <motion.main
+
+                    initial={{ opacity: 0, }}
+                    animate={{ opacity: 1, }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.4, ease: "easeInOut" }}
+                    className="h-full"
+                >
+                    {children}
+                </motion.main>
             </div>
-        </div>
+        </div >
     )
 }
 

dc3e387e4907206494924f74eddfc70c6bb48ab3
Akbar <akbar5yn.developer@gmail.com>
2025-03-14 01:15:08 +0700

    menambahkan animate motion pada route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { Route, Routes } from 'react-router'
+import { Route, Routes, useLocation } from 'react-router'
 import './assets/css/main.css'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import Experience from './pages/Experience'
+import { AnimatePresence } from 'motion/react'
 
 
 function App() {
+  const location = useLocation();
 
   return (
-    <Routes>
-      <Route path='/about' element={<About />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes key={location.pathname} location={location}>
+        <Route path='/about' element={<About />} />
+        <Route path='/experience' element={<Experience />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </AnimatePresence>
   )
 }
 

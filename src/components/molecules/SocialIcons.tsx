f1f926cbcb43614ad5a72025c3382f393a6acd81
Akbar <akbar5yn.developer@gmail.com>
2025-03-27 13:23:13 +0700

    menambahkan target link dan animasi

--- src/components/molecules/SocialIcons.tsx
+++ src/components/molecules/SocialIcons.tsx
@@ -2,13 +2,20 @@ import Icon from "../atoms/Icon"
 import { SiRefinedgithub } from "react-icons/si";
 import { CiLinkedin } from "react-icons/ci";
 import { TiSocialInstagram } from "react-icons/ti";
+import Link from "../atoms/Link";
 
 const SocialIcons = () => {
     return (
         <div className="flex flex-col bg-white rounded-br-lg gap-2 p-2 relative container-image border-r border-b">
-            <Icon><SiRefinedgithub className="w-7 h-7" /></Icon>
-            <Icon><CiLinkedin className="w-7 h-7" /></Icon>
-            <Icon><TiSocialInstagram className="w-7 h-7" /> </Icon>
+            <a href="https://github.com/akbar5yn" target="_blank" rel="noopener noreferrer">
+                <Icon><SiRefinedgithub className="w-7 h-9 transition-all hover:w-8 hover:h-10" /></Icon>
+            </a>
+            <a href="https://www.linkedin.com/in/akbar5yn/" target="_blank" rel="noopener noreferrer">
+                <Icon><CiLinkedin className="w-7 h-7 transition-all hover:w-8 hover:h-8" /></Icon>
+            </a>
+            <a href="https://www.instagram.com/akbar_syn/" target="_blank" rel="noopener noreferrer">
+                <Icon><TiSocialInstagram className="w-7 h-7 transition-all hover:w-8 hover:h-8" /> </Icon>
+            </a>
         </div>
     )
 }

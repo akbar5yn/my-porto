5e44fd517b65c77036496430cb7eb751ddc58d3b
Akbar <akbar5yn.developer@gmail.com>
2025-03-27 13:26:36 +0700

    menambahkan halaman project

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,15 @@
+import Layout from "../components/templates/Layout"
+
+const Experience = () => {
+    return (
+        <main className='lg:h-screen flex items-center justify-center py-20'>
+            <Layout>
+                <div className="flex flex-col h-full gap-5 snap-x">
+
+                </div>
+            </Layout>
+        </main>
+    )
+}
+
+export default Experience

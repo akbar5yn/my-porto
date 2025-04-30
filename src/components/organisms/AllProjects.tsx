7c53b275809a5c4506f4e8c6512b6291c37ea086
akbar5yn <akbar5yn.developer@gmail.com>
2025-04-30 08:57:50 +0700

    membuat halaman project card dan menyesuaikan beberapa tampilan dan data

--- src/components/organisms/AllProjects.tsx
+++ src/components/organisms/AllProjects.tsx
@@ -1,34 +1,56 @@
 import Typography from "../atoms/Typography";
 import ProjectCard from "../molecules/ProjectCard";
-import profile from "../../assets/img/776_1x_shots_so.png"
+import profile from "../../assets/img/lab_management.png"
+import clinicGora from "../../assets/img/clinic_gora_product.png"
+import useEmblaCarousel from "embla-carousel-react";
+import ClassNames from "embla-carousel-class-names";
+import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures'
+import { useCallback } from "react";
+import { FiArrowDownCircle, FiArrowUpCircle } from "react-icons/fi";
 
 const projectItem = [
     {
         image: profile,
         title: "Laboratorium Management",
-        description: "Lasdasdasdasd",
+        description: "Laboratory Management is an application designed to facilitate the operations of a laboratory, specifically for the Physics study program. The application caters to two primary user roles: students and laboratory staff (laboran). It includes several features aimed at streamlining laboratory management and enhancing efficiency for both user types.",
         badges: [
             "Laravel", "Tailwind"
         ]
     },
     {
-        image: profile,
-        title: "Laboratorium Management",
-        description: "Lasdasdasdasd",
+        image: clinicGora,
+        title: "Clinic Gora",
+        description: "Clinic Gora is a digital application specifically designed to support the administrative needs of clinics, with a focus on managing data for Indonesian Migrant Worker (TKI) patients. This application streamlines the processes of patient registration, medical examination documentation, automated receipt generation, and standardized official form (blanko) printing.",
         badges: [
             "Laravel", "Tailwind"
         ]
-    }
+    },
 ]
 
+const wheelGesturesPlugin = WheelGesturesPlugin()
+
 const AllProjects = () => {
+    const [ emblaRef, emblaApi ] = useEmblaCarousel({ axis: 'y', loop: false, watchDrag: true, dragFree: true, align: "start" }, [ ClassNames(), wheelGesturesPlugin ])
+    const scrollPrev = useCallback(() => { if (emblaApi) emblaApi.scrollPrev() }, [ emblaApi ])
+    const scrollNext = useCallback(() => { if (emblaApi) emblaApi.scrollNext() }, [ emblaApi ])
+
     return (
-        <section className="flex flex-col gap-2 text-justify">
+        <section className="flex min-h-[700px] lg:h-full flex-col gap-2">
             <Typography tag="h1" variantClass="title">Projects</Typography>
-            <div className="flex w-full gap-2 h-full">
-                {projectItem.map((project, index) => (
-                    <ProjectCard key={index} {...project} />
-                ))}
+            <div className="relative h-full rounded-xl overflow-hidden inset-shadow-sm">
+                <div className="slider embla project" ref={emblaRef}>
+                    <div className="list embla__container project w-full gap-5 lg:gap-2 grid grid-cols-1 lg:grid-cols-2">
+                        {projectItem.map((project, index) => (
+                            <ProjectCard key={index} className="item emnbla__slide project lg:h-[550px]" {...project} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex gap-2 w-full items-end justify-end">
+                <button className="embla__prev" onClick={scrollPrev}>{<FiArrowUpCircle className="w-7 h-7 " />}
+                </button>
+                <button className="embla__next " onClick={scrollNext}>{<FiArrowDownCircle className="w-7 h-7 " />}</button>
             </div>
         </section>
     );

4013efb5b0c7437da2f9d821ca9fa3947f082fd7
akbar5yn <akbar5yn.developer@gmail.com>
2025-04-30 10:08:17 +0700

    mengubah ukurna padding

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/templates/Layout"
 
 const About = () => {
     return (
-        <main className='lg:h-screen flex items-center justify-center py-20'>
+        <main className='lg:h-screen flex items-center justify-center py-14'>
             <Layout>
                 <div className="flex flex-col h-full gap-5 snap-x">
                     <AboutDescription></AboutDescription>
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/templates/Layout"
 
 const Experience = () => {
     return (
-        <main className='lg:h-screen flex items-center justify-center py-20'>
+        <main className='lg:h-screen flex items-center justify-center py-14'>
             <Layout>
                 <div className="flex flex-col h-full gap-5 snap-x">
                     <ExperienceDeskription></ExperienceDeskription>
